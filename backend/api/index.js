339a875fc22335e0a4f812af227137969fa6a3ba
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-29 14:52:03 +0700

    add login

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { pool } from "../db.js";
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -81,19 +82,63 @@ app.delete("/api/v1/students/:id", async (req, res) => {
 // Add user
 app.post("/api/v1/register", async (req, res) => {
   const { username, password } = req.body;
+  // Validate input
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
   try {
     const hashedPassword = await argon2.hash(password);
     const result = await pool.query(
       "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *",
       [username, hashedPassword]
     );
-    // Send successful response
     res
       .status(201)
       .json({ message: "Registration successful", data: result.rows[0] });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred during registration" });
+  }
+});
+
+// Login user
+app.post("/api/v1/login", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+  try {
+    // Find user by username
+    const result = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username,
+    ]);
+    if (!result.rows[0]) {
+      return res.status(200).json({ error: "Username not found" });
+    } else {
+      // Verify password
+      const isPasswordValid = await argon2.verify(
+        result.rows[0].password,
+        password
+      );
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Incorrect password" });
+      } else {
+        // Send successful response
+        res.status(200).json({
+          message: "Login successful",
+          token: jwt.sign(result.rows[0], process.env.SECRET_KEY),
+          user: result.rows[0],
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred during login" });
   }
 });
 
